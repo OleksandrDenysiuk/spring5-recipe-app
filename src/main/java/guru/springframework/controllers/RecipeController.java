e9f3cffefb02b419e38182c514c444d36ab842b9
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-24 10:20:19 +0300

    Added RecipeController

---
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -0,0 +1,14 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+}
---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,11 @@
+package guru.springframework.controllers;
+
+import org.junit.Before;
+
+public class RecipeControllerTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
