c615574945926e32b7144a009668c967ab9139f4
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-04-24 16:02:48 +0300

    Added commands and converters

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,6 +2,9 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RecipeController {
@@ -10,5 +13,12 @@ public class RecipeController {
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{id}")
+    public String getRecipe(@PathVariable String id,
+                            Model model){
+        model.addAttribute("recipe", recipeService.getRecipeById(Long.valueOf(id)));
+        return "recipe/show";
     }
 }
